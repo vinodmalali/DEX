1921e5733891fd3a45ec2f4654c1211881e9a013
vinodmalali <70831777+vinodmalali@users.noreply.github.com>
2024-01-25 16:21:50 +0530

    Update priceFeed.sol

--- contracts/priceFeed.sol
+++ contracts/priceFeed.sol
@@ -1,253 +1,12 @@
-
-// File: @openzeppelin/contracts/utils/math/SafeMath.sol
-
-
-// OpenZeppelin Contracts (last updated v4.9.0) (utils/math/SafeMath.sol)
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// CAUTION
-// This version of SafeMath should only be used with Solidity 0.8 or later,
-// because it relies on the compiler's built in overflow checks.
-
-/**
- * @dev Wrappers over Solidity's arithmetic operations.
- *
- * NOTE: `SafeMath` is generally not needed starting with Solidity 0.8, since the compiler
- * now has built in overflow checking.
- */
-library SafeMath {
-    /**
-     * @dev Returns the addition of two unsigned integers, with an overflow flag.
-     *
-     * _Available since v3.4._
-     */
-    function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-        unchecked {
-            uint256 c = a + b;
-            if (c < a) return (false, 0);
-            return (true, c);
-        }
-    }
-
-    /**
-     * @dev Returns the subtraction of two unsigned integers, with an overflow flag.
-     *
-     * _Available since v3.4._
-     */
-    function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-        unchecked {
-            if (b > a) return (false, 0);
-            return (true, a - b);
-        }
-    }
-
-    /**
-     * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
-     *
-     * _Available since v3.4._
-     */
-    function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-        unchecked {
-            // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-            // benefit is lost if 'b' is also tested.
-            // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
-            if (a == 0) return (true, 0);
-            uint256 c = a * b;
-            if (c / a != b) return (false, 0);
-            return (true, c);
-        }
-    }
-
-    /**
-     * @dev Returns the division of two unsigned integers, with a division by zero flag.
-     *
-     * _Available since v3.4._
-     */
-    function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-        unchecked {
-            if (b == 0) return (false, 0);
-            return (true, a / b);
-        }
-    }
-
-    /**
-     * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
-     *
-     * _Available since v3.4._
-     */
-    function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
-        unchecked {
-            if (b == 0) return (false, 0);
-            return (true, a % b);
-        }
-    }
-
-    /**
-     * @dev Returns the addition of two unsigned integers, reverting on
-     * overflow.
-     *
-     * Counterpart to Solidity's `+` operator.
-     *
-     * Requirements:
-     *
-     * - Addition cannot overflow.
-     */
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a + b;
-    }
-
-    /**
-     * @dev Returns the subtraction of two unsigned integers, reverting on
-     * overflow (when the result is negative).
-     *
-     * Counterpart to Solidity's `-` operator.
-     *
-     * Requirements:
-     *
-     * - Subtraction cannot overflow.
-     */
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a - b;
-    }
-
-    /**
-     * @dev Returns the multiplication of two unsigned integers, reverting on
-     * overflow.
-     *
-     * Counterpart to Solidity's `*` operator.
-     *
-     * Requirements:
-     *
-     * - Multiplication cannot overflow.
-     */
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a * b;
-    }
-
-    /**
-     * @dev Returns the integer division of two unsigned integers, reverting on
-     * division by zero. The result is rounded towards zero.
-     *
-     * Counterpart to Solidity's `/` operator.
-     *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
-     */
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a / b;
-    }
-
-    /**
-     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
-     * reverting when dividing by zero.
-     *
-     * Counterpart to Solidity's `%` operator. This function uses a `revert`
-     * opcode (which leaves remaining gas untouched) while Solidity uses an
-     * invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
-     */
-    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-        return a % b;
-    }
-
-    /**
-     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
-     * overflow (when the result is negative).
-     *
-     * CAUTION: This function is deprecated because it requires allocating memory for the error
-     * message unnecessarily. For custom revert reasons use {trySub}.
-     *
-     * Counterpart to Solidity's `-` operator.
-     *
-     * Requirements:
-     *
-     * - Subtraction cannot overflow.
-     */
-    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        unchecked {
-            require(b <= a, errorMessage);
-            return a - b;
-        }
-    }
-
-    /**
-     * @dev Returns the integer division of two unsigned integers, reverting with custom message on
-     * division by zero. The result is rounded towards zero.
-     *
-     * Counterpart to Solidity's `/` operator. Note: this function uses a
-     * `revert` opcode (which leaves remaining gas untouched) while Solidity
-     * uses an invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
-     */
-    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        unchecked {
-            require(b > 0, errorMessage);
-            return a / b;
-        }
-    }
-
-    /**
-     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
-     * reverting with custom message when dividing by zero.
-     *
-     * CAUTION: This function is deprecated because it requires allocating memory for the error
-     * message unnecessarily. For custom revert reasons use {tryMod}.
-     *
-     * Counterpart to Solidity's `%` operator. This function uses a `revert`
-     * opcode (which leaves remaining gas untouched) while Solidity uses an
-     * invalid opcode to revert (consuming all remaining gas).
-     *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
-     */
-    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-        unchecked {
-            require(b > 0, errorMessage);
-            return a % b;
-        }
-    }
-}
-
-// File: @chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol
-
-
-pragma solidity ^0.8.0;
-
-interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
-
-  function description() external view returns (string memory);
-
-  function version() external view returns (uint256);
-
-  function getRoundData(
-    uint80 _roundId
-  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-
-  function latestRoundData()
-    external
-    view
-    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-}
-
-// File: sakhaSwap/priceFeed.sol
-
-
-pragma solidity ^0.8.0;
-
-
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract PriceFetcher {
     using SafeMath for uint256;
+    using SafeMath for int;
 
     address owner;
 
@@ -300,11 +59,36 @@ contract PriceFetcher {
         return price;
     }
 
-    function getAmtSg(string memory tokenASymbol,uint amountADesired,string memory tokenBSymbol, uint amountBDesired) external view returns (int) {
+    // function getAmtSg(string memory tokenASymbol,uint amountADesired,string memory tokenBSymbol, uint amountBDesired) external view returns (int) {
+    //     address assetAadr = symbolToAddr[tokenASymbol];
+    //     address assetBadr = symbolToAddr[tokenBSymbol];
+    //    (, int priceA, , ,) = AggregatorV3Interface(assetAadr).latestRoundData();
+    //    (, int priceB, , ,) = AggregatorV3Interface(assetBadr).latestRoundData();
+
+    //     uint256 totalUSD = (amountADesired.div(1e18)).mul(uint256(priceA)).add((amountBDesired.div(1e18)).mul(uint256(priceB)));
+    //     int amtSG = int(totalUSD.div(rewardUnit).div(1e8));
+
+    //     return amtSG;
+    // }
+
+    function getAmtSg(string memory tokenASymbol, uint amountADesired, string memory tokenBSymbol, uint amountBDesired) external view returns (int) {
         address assetAadr = symbolToAddr[tokenASymbol];
-        address assetBadr = symbolToAddr[tokenBSymbol];
-       (, int priceA, , ,) = AggregatorV3Interface(assetAadr).latestRoundData();
-       (, int priceB, , ,) = AggregatorV3Interface(assetBadr).latestRoundData();
+        address assetBadr;
+        int priceA;
+        int priceB;
+
+        (, priceA, , ,) = AggregatorV3Interface(assetAadr).latestRoundData();
+
+        // Check if amountBDesired is zero
+        if (amountBDesired == 0) {
+            // Set priceB to zero and assetBadr to address(0)
+            priceB = 0;
+            assetBadr = address(0);
+        } else {
+            // Get assetBadr and priceB if amountBDesired is not zero
+            assetBadr = symbolToAddr[tokenBSymbol];
+            (, priceB, , ,) = AggregatorV3Interface(assetBadr).latestRoundData();
+        }
 
         uint256 totalUSD = (amountADesired.div(1e18)).mul(uint256(priceA)).add((amountBDesired.div(1e18)).mul(uint256(priceB)));
         int amtSG = int(totalUSD.div(rewardUnit).div(1e8));
